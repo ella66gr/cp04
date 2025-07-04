d3c997edd55a72e5a79e3a61b907a1f35546b31d
Ella Green <12546112+ella66gr@users.noreply.github.com>
2025-07-04 10:56:21 +0100

    Top input section updated.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,21 +60,19 @@
    
   <div class="bg-white dark:bg-gray-800 border-l border-r border-b border-gray-200 dark:border-gray-700 mt-0">
 
-
-
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
 
-      <!-- Main input fields  -->
+      <!-- Top section input fields  -->
 
       <form>
-        <div class="mb-6 grid gap-6 md:grid-cols-2">
+        <div class="mb-6 grid gap-8 md:grid-cols-2">
           <div>
             <Label for="profile_name" class="mb-2">Profile Name</Label>
-            <Input type="text" id="first_name" placeholder="Name" required class="mb-4"/>
+            <Input type="text" id="first_name" placeholder="Name" required class="mb-6"/>
             <Label for="profile_description" class="mb-2">Profile Description</Label>
-            <Textarea class="mb-4" label="Description" placeholder="Optional profile description …" bind:value={profile_description} rows={3} />
+            <Textarea class="mb-6" label="Description" placeholder="Optional profile description and purpose …" bind:value={profile_description} rows={3} />
             <Label for="criterion1" class="mb-2">Tone of Voice</Label>
-              <Select class="mb-4" label="Tone of Voice" placeholder="Select tone of voice" bind:value={tone_of_voice}>
+              <Select class="mb-6" label="Tone of Voice" placeholder="Select tone of voice" bind:value={tone_of_voice}>
                 <option value="professional_supportive">Professional / Supportive</option>
                 <option value="empowering_activist">Empowering / Activist</option>
                 <option value="community_caring">Community & Caring</option>
@@ -82,65 +80,64 @@
           </div>
 
           <div>
+
+            <!-- EVALUATION CRITERIA -->
+
             <Label for="criterion1" class="mb-2">Evaluation Criteria for Sources</Label>
-              <Select class="mb-4 text-red-600" label="Evaluation Criteria" placeholder="Select evaluation criteria" bind:value={criterion1}>
+              <Select class="mb-6 text-red-600" label="Evaluation Criteria" placeholder="Select evaluation criteria" bind:value={criterion1}>
                 <option value="articleQuality">Article Quality</option>
                 <option value="genderSenseAlignment">GenderSense Alignment</option>
                 <option value="warm">Empowering & Positive</option>
                 <option value="currentUpToDate">Current & up to date</option>
               </Select>
   
-              <Select  class="mb-4" label="Evaluation Criteria" placeholder="Select evaluation criteria" bind:value={criterion2}>
+              <Select  class="mb-6" label="Evaluation Criteria" placeholder="Select evaluation criteria" bind:value={criterion2}>
                 <option value="articleQuality">Article Quality</option>
                 <option value="genderSenseAlignment">GenderSense Alignment</option>
                 <option value="warm">Empowering & Positive</option>
                 <option value="currentUpToDate">Current & up to date</option>
               </Select>
 
-              <Select  class="mb-4" label="Evaluation Criteria"  placeholder="Select evaluation criteria" bind:value={criterion3}>
+              <Select  class="mb-6" label="Evaluation Criteria"  placeholder="Select evaluation criteria" bind:value={criterion3}>
                 <option value="articleQuality">Article Quality</option>
                 <option value="genderSenseAlignment">GenderSense Alignment</option>
                 <option value="warm">Empowering & Positive</option>
                 <option value="currentUpToDate">Current & up to date</option>
               </Select>
+
+              <!-- CATEGORY TAGS -->
+
+              <h5 class="mt-8 mb-2 text-sm">Available Category Tags (e.g. newsletter sections)</h5>
+              <Card class="p-4 sm:p-6 md:p-8 min-w-full">
+                        
+              <Checkbox class="" aria-describedby="helper-checkbox-text">In the News</Checkbox>
+              <Helper id="helper-checkbox-text" class="ps-7 mb-4">News items of general Trans+ interest</Helper>
+
+              <Checkbox aria-describedby="helper-checkbox-text">Trans+ Health</Checkbox>
+              <Helper id="helper-checkbox-text" class="ps-7 mb-4">Trans+ health tips and info</Helper>
+
+              <Checkbox aria-describedby="helper-checkbox-text">GenderSense Latest</Checkbox>
+              <Helper id="helper-checkbox-text" class="ps-7 mb-4">Updates on what is going on at GenderSense</Helper>
+
+              <Checkbox aria-describedby="helper-checkbox-text">Transition Coaching Tips</Checkbox>
+              <Helper id="helper-checkbox-text" class="ps-7 mb-4">Hints and tips on making life easier</Helper>
+
+              <Checkbox aria-describedby="helper-checkbox-text">GenderSense Community Highlights</Checkbox>
+              <Helper id="helper-checkbox-text" class="ps-7 mb-4">Items from the GenderSense online community</Helper>
+
+              <Checkbox aria-describedby="helper-checkbox-text">Trans+ Issues & Rights Updates</Checkbox>
+              <Helper id="helper-checkbox-text" class="ps-7 mb-4">Community issues</Helper>
+
+              </Card>
+
           </div>
 
-          <div>
-            <Label for="description" class="mb-2">Description</Label>
-            <Input type="text" id="company" placeholder="Flowbite" required />
-          </div>
-          <div>
-            <Label for="phone" class="mb-2">Phone number</Label>
-            <Input type="tel" id="phone" placeholder="123-45-678" pattern={"[0-9]{3}-[0-9]{2}-[0-9]{3}"} required />
-          </div>
-          <div>
-            <Label for="website" class="mb-2">Website URL</Label>
-            <Input type="url" id="website" placeholder="flowbite.com" required />
-          </div>
-          <div>
-            <Label for="visitors" class="mb-2">Unique visitors (per month)</Label>
-            <Input type="number" id="visitors" placeholder="" required />
-          </div>
         </div>
-        <div class="mb-6">
-          <Label for="email" class="mb-2">Email address</Label>
-          <Input type="email" id="email" placeholder="john.doe@company.com" required />
-        </div>
-        <div class="mb-6">
-          <Label for="password" class="mb-2">Password</Label>
-          <Input type="password" id="password" placeholder="•••••••••" required />
-        </div>
-        <div class="mb-6">
-          <Label for="confirm_password" class="mb-2">Confirm password</Label>
-          <Input type="password" id="confirm_password" placeholder="•••••••••" required />
-        </div>
-        <Checkbox divClass="mb-6 gap-1 rtl:space-x-reverse" required>
-          I agree with the <A href="/" class="text-primary-700 dark:text-primary-600 hover:underline">terms and conditions</A>.
-        </Checkbox>
-        <Button type="submit">Submit</Button>
+
       </form>
 
-      <!-- Grid section -->
+      <!-- Bottom grid section -->
+
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 mb-8">
         <div>
           <Card class="p-4 sm:p-6 md:p-8 min-w-full">
@@ -157,8 +154,6 @@
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CARD 2. Noteworthy technology acquisitions 2021</h5>
           <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
           </Card>
-
-          
 
         </div>
 
@@ -194,12 +189,7 @@
         </Card>
       </div>
 
-      <div>
-        <Card class="p-4 sm:p-6 md:p-8">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CARD 6. Noteworthy technology acquisitions 2021</h5>
-          <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </Card>
-      </div>
+
 
     </div>
 
